ca66d9913321e3bae038b34a499798f944af8508
Akshay Vhatkar <90630041+Akshay-0706@users.noreply.github.com>
2022-02-02 15:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,94 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+from tensorflow.keras.models import load_model 
 import streamlit as st
-from datetime import date
-
-import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
-
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
-st.set_page_config(layout="wide")
-
-st.title('Stock Forecast App')
-
-selected_stock = st.text_input('Select dataset for prediction', 'GOOG')
-
-n_years = st.slider('Days of prediction:', 100, 500)
-period = n_years 
 
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
 
-	
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
+start = "2010-01-01"
+end = "2019-12-31"
 
-st.subheader('Raw data')
-st.write(data.tail())
+st.title('Stock Trend Prediction')
 
-# Plot raw data
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Range Slider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+user_input = st.text_input('Enter Stock Ticker', 'AAPL')
+df = data.DataReader(user_input, 'yahoo', start, end)
 
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+# Describing data
+st.subheader('Data from 2010-2019')
+st.write(df.describe())
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+# visualistion
+st.subheader('Closing Price vs Time Chart')
+fig =plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
 
-# Show and plot forecast
+st.subheader('Closing Price vs Time Chart with 100MA')
+ma100 =df.Close.rolling(100).mean()
+fig =plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(df.Close)
+st.pyplot(fig)
 
-st.subheader('Forecast data')
-st.write(forecast.tail())
+st.subheader('Closing Price vs Time Chart with 200MA')
+ma100 =df.Close.rolling(100).mean()
+ma200 =df.Close.rolling(200).mean()
+fig =plt.figure(figsize=(12,6))
+plt.plot(ma100,'r')
+plt.plot(ma200,'g')
+plt.plot(df.Close,'b')
+st.pyplot(fig)
 
 
-st.subheader(f'Forecast plot for {n_years} days')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
 
-st.subheader("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+# spliting data into training and testing
+
+data_training =pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing =pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+# scaling down data in 0,1 scale
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array= scaler.fit_transform(data_training)
+
+
+
+model = load_model('keras.model.h5')
+
+
+
+
+# testing part
+past_100_days= data_training.tail(100)
+final_df = past_100_days.append(data_testing,ignore_index = True)
+input_data =scaler.fit_transform(final_df)
+
+x_test =[]
+y_test =[]
+
+for i in range(100,input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted =y_predicted*scale_factor
+y_test =y_test*scale_factor
+
+
+
+# final graph
+st.subheader('Predictions vs Orignal')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test,'b',label ='Original Price')
+plt.plot(y_predicted,'r',label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
